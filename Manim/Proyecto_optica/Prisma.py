5a1370dd52dc7cd264fd6e7d9224f463fde1411b
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-04-14 02:12:53 -0600

    2 escenas

--- Manim/Proyecto_optica/Prisma.py
+++ Manim/Proyecto_optica/Prisma.py
@@ -8,6 +8,7 @@
 
 from big_ol_pile_of_manim_imports import *
 import numpy as np
+
 class prisma(GraphScene):
     CONFIG={
         "x_min": -20,
@@ -23,62 +24,82 @@ class prisma(GraphScene):
     def show_function_graph(self):
         self.setup_axes(animate=False)
         # Math function
-        valinic=1.1
-        deltava=0.01
-        valdivi=2.5
-        deltadi=0.25
+        valin=1.1
+        delva=0.01
+        valdi=2.5
+        deldi=0.25
         def func0(x):
             return 0.15**2*x+0.25
         def func1(x):
-            return (np.sin(valinic*x)+x/(valdivi)**2)/3
+            return (np.sin(valin*x)+x/(valdi)**2)/3
         def func2(x):
-            return (np.sin((valinic+deltava)*x)+x/(valdivi+deltadi)**2)/3
+            return (np.sin((valin+delva)*x)+x/(valdi+deldi)**2)/3
         def func3(x):
-            return (np.sin((valinic+deltava*2)*x)+x/(valdivi+deltadi*2)**2)/3
+            return (np.sin((valin+delva*2)*x)+x/(valdi+deldi*2)**2)/3
         def func4(x):
-            return (np.sin((valinic+deltava*3)*x)+x/(valdivi+deltadi*3)**2)/3
+            return (np.sin((valin+delva*3)*x)+x/(valdi+deldi*3)**2)/3
         def func5(x):
-            return (np.sin((valinic+deltava*4)*x)+x/(valdivi+deltadi*4)**2)/3
+            return (np.sin((valin+delva*4)*x)+x/(valdi+deldi*4)**2)/3
         def func6(x):
-            return (np.sin((valinic+deltava*5)*x)+x/(valdivi+deltadi*5)**2)/3
+            return (np.sin((valin+delva*5)*x)+x/(valdi+deldi*5)**2)/3
         def func7(x):
-            return (np.sin((valinic+deltava*6)*x)+x/(valdivi+deltadi*6)**2)/3
+            return (np.sin((valin+delva*6)*x)+x/(valdi+deldi*6)**2)/3
+        def func8(x):
+            return 2*np.sin(0.3*x)
         def Triangulo1(x):
             return -1
         def Triangulo2(x):
             return 0.2*x+1
         def Triangulo3(x):
             return -0.2*x+1                
-        linea1=self.get_graph(Triangulo1,x_min=-10,x_max=10)
-        linea1.set_color(LIGHT_GRAY)
-        linea2=self.get_graph(Triangulo2,x_min=-10,x_max=0)
-        linea2.set_color(LIGHT_GRAY)
-        linea3=self.get_graph(Triangulo3,x_min=0,x_max=10)
-        linea3.set_color(LIGHT_GRAY)
+
+        lt1=self.get_graph(Triangulo1,x_min=-10,x_max=10, color=LIGHT_GRAY)
+        lt2=self.get_graph(Triangulo2,x_min=-10,x_max=0, color=LIGHT_GRAY)
+        lt3=self.get_graph(Triangulo3,x_min=0,x_max=10, color=LIGHT_GRAY)
+        
         x0=-4
         xf=20*np.pi
-        graph0=self.get_graph(func0,x_min=-20,x_max=-4, color=WHITE)
-        graph1=self.get_graph(func1,x_min=x0,x_max=xf, color=RED)
-        graph2=self.get_graph(func2,x_min=x0,x_max=xf, color=ORANGE)
-        graph3=self.get_graph(func3,x_min=x0,x_max=xf, color=YELLOW)
-        graph4=self.get_graph(func4,x_min=x0,x_max=xf, color=GREEN)
-        graph5=self.get_graph(func5,x_min=x0,x_max=xf, color=BLUE_C)
-        graph6=self.get_graph(func6,x_min=x0,x_max=xf, color=PURPLE_E)
-        graph7=self.get_graph(func7,x_min=x0,x_max=xf, color=PURPLE_C)
+
+        lu0=self.get_graph(func0,x_min=-20,x_max=-4, color=WHITE)
+        lu1=self.get_graph(func1,x_min=x0,x_max=xf, color=RED)
+        lu2=self.get_graph(func2,x_min=x0,x_max=xf, color=ORANGE)
+        lu3=self.get_graph(func3,x_min=x0,x_max=xf, color=YELLOW)
+        lu4=self.get_graph(func4,x_min=x0,x_max=xf, color=GREEN)
+        lu5=self.get_graph(func5,x_min=x0,x_max=xf, color=BLUE_C)
+        lu6=self.get_graph(func6,x_min=x0,x_max=xf, color=PURPLE_E)
+        lu7=self.get_graph(func7,x_min=x0,x_max=xf, color=PURPLE_C)
         
-        self.play(ShowCreation(linea1),
-            ShowCreation(linea2),
-            ShowCreation(linea3))
-            #run_time=1)        
-        self.play(ShowCreation(graph0),
+        senl=self.get_graph(func8,x_min=2*x0*np.pi,x_max=xf, color=WHITE)
+        
+        self.play(ShowCreation(lt1),
+            ShowCreation(lt2),
+            ShowCreation(lt3),
+            run_time=0)        
+        self.play(ShowCreation(lu0),
             run_time=3)
-        self.play(ShowCreation(graph1),
-            ShowCreation(graph2),
-            ShowCreation(graph3),
-            ShowCreation(graph4),
-            ShowCreation(graph5),
-            ShowCreation(graph6),
-            ShowCreation(graph7),
+        self.play(ShowCreation(lu1),
+            ShowCreation(lu2),
+            ShowCreation(lu3),
+            ShowCreation(lu4),
+            ShowCreation(lu5),
+            ShowCreation(lu6),
+            ShowCreation(lu7),
             run_time=6)
+        self.wait(5)
+        self.play(FadeOut(lt1),
+            FadeOut(lt2),
+            FadeOut(lt3),
+            FadeOut(lu1),
+            FadeOut(lu2),
+            FadeOut(lu3),
+            FadeOut(lu4),
+            FadeOut(lu5),
+            FadeOut(lu6),
+            FadeOut(lu7),
+            run_time=1)
+        self.play(Transform(lu0, senl),
+            run_time=3)
+        self.wait(5)
     def construct(self):
         self.show_function_graph()
+
