549ab475494b28094bee947e9ffed1a21d9b61bc
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-07-26 03:06:14 -0600

    Análisis del péndulo doble
    Archivos generados y programa

--- Pendulo Doble/PenduloDoble.py
+++ Pendulo Doble/PenduloDoble.py
@@ -1,18 +1,43 @@
-# Programa para el pendulo doble
+'''
+2021-07-26
+PenduloDoble.py
+Pablo Martínez (pabloversion1.0@gmail.com)
 
+Programa que calcula candidatos a condiciones iniciales que provean una trayectoria
+cercana a la observada en un video del péndulo doble.
+
+Copyright (C) 2021
+P. D. Martínez Zeceña
+pabloversion1.0@gmail.com
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
+'''
+##############################################################################################
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import animation
-import os
 import time
 inicio= time.time()
 
-archivo=pd.ExcelFile("datos.xlsx")
-Prec_Simple=archivo.parse("Normalizado_S")
-Prec_Total=archivo.parse("Normalizado_T")
-Radio_S=archivo.parse("radio_S")
-Radio_T=archivo.parse("radio_T")
+archivo=pd.ExcelFile("datos.xlsx")          # Archivo donde se leen los datos obtenidos
+Prec_Simple=archivo.parse("Normalizado_S")  # Debe existir una hoja con datos normalizados de 
+Prec_Total=archivo.parse("Normalizado_T")   # precisión simple y otra con precisión total
+Radio_S=archivo.parse("radio_S")            # Relaciones de proporcionalidad de la longitud relativa
+Radio_T=archivo.parse("radio_T")            # entre ambas varillas, con precisión simple y total
+
+##############################################################################################
+# Funciones para poder comparar las filas de los datos del video vs los datos de la simulación
 
 def xro(fila,caso,archi1,archi2):     # Caso 1 precisión simple, Caso 2 precisión total
         if caso==1:
@@ -38,31 +63,37 @@ def yaz(fila,caso,archi1,archi2):
         elif caso==2:
             return archi2.loc[fila][4]
 
-def comparar(archivo1,archivo2,archivo3,a,delta,porcentaje,caso):      # Archivo1=Simple, Archivo2=Total, Archivo3=Runge-Kutta
+# Función para comparar arreglos de datos
+# Archivo1=Simple, Archivo2=Total, Archivo3=Runge-Kutta
+def comparar(archivo1,archivo2,archivo3,a,delta,porcentaje,caso):
     total=1
     acierto=1
-    for i in np.arange(0,a,5):
+    for i in np.arange(0,a,2):
         total+=1
-        if (xaz(i,caso,archivo1,archivo2)-xaz(i,caso,archivo3,archivo3))<delta:
-            acierto+=1
+        if abs((xaz(i,caso,archivo1,archivo2)-xaz(i,caso,archivo3,archivo3)))<delta:
+            if abs((xro(i,caso,archivo1,archivo2)-xro(i,caso,archivo3,archivo3)))<delta:
+                acierto+=1
     return (100*acierto)/total
 
+# cálculo de la constante de proporcionalidad entre ambas varillas
 def relativo(caso,archi1,archi2):
     return yro(0,caso,archi1,archi2)/xro(0,caso,archi1,archi2)
-###############################################################################
-# Encontrar theta1 y theta2 iniciales
+
+##############################################################################################
+# Encontrar theta1 y theta2 iniciales en un rango determinado
 
 theta_0=[]
 theta1_0=[]
 theta2_0=[]
-for theta1 in np.arange(180,270.5,0.1):
+
+for theta1 in np.arange(200,210,0.2):
     x1=54*np.sin(np.radians(theta1))
     xvaluar=xro(0,1,Prec_Simple,Prec_Total)*100
     porcent=xvaluar*0.01
     if  x1>xvaluar+porcent and x1<xvaluar-porcent:
         theta_0.append(theta1)
 for theta11 in(theta_0):
-    for theta2 in np.arange(0,90,0.1):
+    for theta2 in np.arange(15,25,0.2):
         x2=54*np.sin(np.radians(theta11))+45*np.sin(np.radians(theta2))
         x2valuar=xaz(0,1,Prec_Simple,Prec_Total)*100
         porcent2=xvaluar*0.01
@@ -70,7 +101,9 @@ for theta11 in(theta_0):
             theta2_0.append(theta2)
             theta1_0.append(theta11)
             
-###############################################################################
+##############################################################################################
+# Inicia código modificado de Ahmed Alkharusi, estas modificaciones fueron para
+# adecuar el código según las necesidades del problema
 """
 # =============================================================================
 # Simulating the double pendulum using Runge–Kutta method (RK4)
@@ -129,46 +162,41 @@ def rk4(deriv,func_i,func_i2, x_i,h):
     x = x_i + h
     return (x,func)
 
+##############################################################################################
 
-
-c=0
-ite=0
-
-theta1_salida=[]
+ite=0                       # Variable de conteo sobre las iteraciones realizadas
+theta1_salida=[]            # Arreglos para almacenar variables necesarias
 theta2_salida=[]
 m1_salida=[]
 r1_salida=[]
 porcentaje_salida=[]
 
-
-cant_p = 900                              # Cantidad de pasos
-g = 9.81                                # gravedad
+cant_p = 300                            # Cantidad de pasos a realizar
+g = 981                                 # cm/s^2 gravedad
 h = 0.008333333333                      # Tamaño de la iteración para el método RK4
 relacion=relativo(1,Radio_S,Radio_T)    # Constante de proporcinalidad
-
-for aa in range(0,len(theta1_0)):       # Variar para cada caso de ángulos iniciales
-    for a in np.arange(0.3,2,0.3):      # Se varía el valor de ambas barillas
-        for b in np.arange(0.3,5,0.1):    # y también la longitud de las masas
+for aa in range(0,len(theta1_0)):       # Variar para cada pareja de candidatos de los de ángulos iniciales
+    for a in np.arange(5,15,0.5):       # Se varía el valor de ambas barillas
+        for b in np.arange(0.1,6,0.2):  # y también la longitud de las masas
             
             # Condiciones iniciales
-            m1 = b      # masa del péndulo rojo
-            m2 = m1     # asumimos que es la misma masa
-            r1 = a      # longitud pendulo rojo
-            r2 = r1*relacion     # asumimos misma longitud
-            runit = np.sqrt(r1**2+r2**2)        # Se normaliza para poder trabajar independiente de la distancia
+            m1 = b                      # masa del péndulo rojo
+            m2 = m1                     # asumimos que es la misma masa
+            r1 = a                      # longitud péndulo rojo
+            r2 = r1*relacion            # longitud péndulo azul
+            runit = np.sqrt(r1**2+r2**2)  # Se normaliza para poder trabajar independiente de la distancia
             x = y = []
           
             # Condiciones iniciales de los ángulos [ángulo, velocidad angular]
             a1_arr = np.array([np.radians(theta1_0[aa]),0])
             a2_arr = np.array([np.radians(theta2_0[aa]),0])
             t = 0                           # Tiempo inicial
-            
-            steps_no = cant_p               # number of steps of the RK4 method
+            steps_no = cant_p
             time_arr = np.array([t])
             func_array1 = np.array([a1_arr])
             func_array2 = np.array([a2_arr])
             
-            tiempo=[]
+            tiempo=[]                   # Arreglo para añadir tiempo al data frame
             tiempo.clear()
             tiempo.append(0)
             for i in range(steps_no):
@@ -180,7 +208,7 @@ for aa in range(0,len(theta1_0)):       # Variar para cada caso de ángulos inic
                 func_array1 = np.vstack((func_array1,np.array([a1_arr])))
                 func_array2 = np.vstack((func_array2,np.array([a2_arr])))
                 tiempo.append(t)
-                ite+=1
+                ite+=1                  # Aumenta la cantidad de iteraciones
             
             # You can plot the pendulum's position or angular speed/acceleration as a function of time
             [pendulum1_theta, pendulum1_angular_speed] = func_array1.transpose()
@@ -194,81 +222,39 @@ for aa in range(0,len(theta1_0)):       # Variar para cada caso de ángulos inic
             
             np.true_divide(pendulum1_x,runit)
             
+            # Se crea un dataframe con los datos obtenidos en este caso
             dataf = pd.DataFrame({"t":tiempo,
                                   "xr":pendulum1_x,
                                   "yr":pendulum1_y,
                                   "xa":pendulum2_x,
                                   "ya":pendulum2_y,})
-    
-            c+=1                        # Se aumenta en uno con cada éxito y se crea un archivo distinto
             
-            
-            porcentaje = comparar(Prec_Simple,Prec_Total,dataf,cant_p,0.01,10,1)
-            if porcentaje > 50:
-                #print("aciertos:",porcentaje,"caso",c)
-                #print("theta1=",theta1_0[aa],"theta2=",theta2_0[aa],"m1=",m1,"r1=",r1)
-                theta1_salida.append(theta1_0[aa])
+            # Se compara el resultado de la simulación contra los datos obtenidos            
+            porcentaje = comparar(Prec_Simple,Prec_Total,dataf,cant_p,0.05,10,1)
+            if porcentaje > 50:         # Si se desea solamente resultados sobre cierto porcentaje
+                theta1_salida.append(theta1_0[aa])  # Se almacenan las cond. iniciales que cumplan
                 theta2_salida.append(theta2_0[aa])
                 m1_salida.append(m1)
                 r1_salida.append(r1)
                 porcentaje_salida.append(porcentaje)
-                
-                
-            '''
-            # Graficar el cada frame del movimiento
-            scatter_x = []
-            scatter_y = []
-            counter = 0
-            save_every_n_frames = 5
-            for j in range(int(len(pendulum1_y)/save_every_n_frames)):
-                i = j*save_every_n_frames
-                fig = plt.figure()
-                ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2,2), ylim=(-2.1, 1.5))
-                x = [0, pendulum1_x[i]]
-                y = [0, pendulum1_y[i]]
-                ax.plot(x,y,lw=3,color='red')
-                x1 = [pendulum1_x[i], pendulum2_x[i]]
-                y1 = [pendulum1_y[i], pendulum2_y[i]]
-                scatter_x.append(pendulum2_x[i])
-                scatter_y.append(pendulum2_y[i])
-                ax.plot(x1,y1,'o-',lw=3,color='blue',markersize=5)
-                ax.scatter(scatter_x,scatter_y,lw=0.0005,color='black')
-                ax.set_xlabel('$x-Axis$',fontsize=12)
-                ax.set_ylabel('$y-Axis$',fontsize=12)
-                ax.set_title('Péndulo Doble Runge-Kutta ',fontsize=14)
-                ax.grid()
-                fig.savefig(str(j)+'.png',dpi=300)
-                plt.show()
-            '''
-            ############################################################################################        
-            # Guardar datos en excel
-            '''
-            k="salida"+str(c)+".xlsx"
-            writer=pd.ExcelWriter(k)
-            dataf.to_excel(writer,index=False)
-            #dataf.to_excel(writer)
-            writer.save()
-            '''
+        
+##############################################################################################
+# Se crea un dataframe con los casos que superaron el límite anterior
 cond_inic_salida=pd.DataFrame({"porcentaje":porcentaje_salida,
                                "theta1": theta1_salida,
                                "theta2": theta2_salida,
                                "m1": m1_salida,
                                "r1": r1_salida})            
 
+# Para evitar realizar operaciones en excel, se ordenan los resultados de mayor a menor
 cond_inic_salida=cond_inic_salida.sort_values(by=['porcentaje'])
 cond_inic_salida=cond_inic_salida[::-1]
-k2="condiciones.xlsx"
+k2="condiciones.xlsx"   # El archivo de excel con los resultados se llama "condiciones"
 writer2=pd.ExcelWriter(k2)
 cond_inic_salida.to_excel(writer2,index=False)
 writer2.save()            
-############################################################################################
-'''
-# Eliminar los archivos de excel
-for i in range(1,c+1):
-    k2="salida"+str(i)+".xlsx"
-    os.remove(k2)
-'''    
+##############################################################################################
+# Se muestran el tiempo de ejecución y la cantidad de iteraciones
 print("iteraciones=",ite)
 fin = time.time()
 print("tiempo=",fin-inicio,"s")
-print("tiempo=",(fin-inicio)/60,"m")
